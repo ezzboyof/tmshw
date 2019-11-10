54f375fb1a1a644676eb3458696e12b00009224c
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-10 15:29:41 +0300

    Update Task2.java

--- src/homework_9/task_2/Task2.java
+++ src/homework_9/task_2/Task2.java
@@ -10,11 +10,11 @@ public class Task2 {
 
     public static void main(String[] args) {
         try (FileWriter fw = new FileWriter(FILE_PATH, true)) {
-            for (int i = 0; i < 10000; i++) {
+            for (int i = 0; i < 10000; i++) { // 10_000 в переменную 
                 fw.write("10000 text");
                 fw.write("\n");
             }
-
+// пустая строка не нужна
             fw.flush();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
