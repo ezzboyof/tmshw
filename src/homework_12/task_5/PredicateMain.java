b04cbb443cbb34da92f70e8a0f72596ab2a15f80
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-19 23:19:02 +0300

    Update PredicateMain.java

--- src/homework_12/task_5/PredicateMain.java
+++ src/homework_12/task_5/PredicateMain.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.function.Predicate;
 
+// +
 public class PredicateMain {
     public static void main(String[] args) {
         List<User> users = new ArrayList<>();
