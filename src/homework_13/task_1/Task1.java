f14babaee7015afadd0ad81f41837a71e7e31b89
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-22 21:41:57 +0300

    Update Task1.java

--- src/homework_13/task_1/Task1.java
+++ src/homework_13/task_1/Task1.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+// +
 public class Task1 {
     public static void main(String[] args) {
         List<String> listString = new ArrayList<>();
