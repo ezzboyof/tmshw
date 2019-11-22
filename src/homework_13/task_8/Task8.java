47a35f47ab72e4d81bbe3cb3b4ab8684014144e0
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-22 21:47:48 +0300

    Update Task8.java

--- src/homework_13/task_8/Task8.java
+++ src/homework_13/task_8/Task8.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+// +
 public class Task8 {
     public static void main(String[] args) {
         List<Phone> phones = new ArrayList<>();
