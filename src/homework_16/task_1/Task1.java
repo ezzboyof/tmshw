362a49fd22acbe3f42da2cec8f2bd8eae24473b2
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-12-03 23:06:17 +0300

    Update Task1.java

--- src/homework_16/task_1/Task1.java
+++ src/homework_16/task_1/Task1.java
@@ -4,5 +4,6 @@ public class Task1 {
     public static void main(String[] args) {
         MyThread customThread = new MyThread("MyThread");
         customThread.start();
+        // не стоит забывать про метод join()
     }
 }
