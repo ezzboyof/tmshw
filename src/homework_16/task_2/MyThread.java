622243956863e16200636b1ade100725f8afc387
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-12-03 23:07:32 +0300

    Update MyThread.java

--- src/homework_16/task_2/MyThread.java
+++ src/homework_16/task_2/MyThread.java
@@ -7,10 +7,11 @@ public class MyThread extends Thread {
 
     @Override
     public void run() {
+        // 21 лучше через конструктор
         for (int i = 0; i < 21; i++) {
             System.out.println(i);
             try {
-                Thread.sleep(1);
+                Thread.sleep(1);// метод sleep() принимает время в миллисекундах, получается поток у тебя спит 1 миллисекунду, а надо секунду
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
